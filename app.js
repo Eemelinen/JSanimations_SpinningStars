b8d09e996542c1f510977c035c582be90daf066b
Eemeli Surakka <eemeli.surakka@hotmail.fi>
2020-10-02 14:38:19 +0200

    stars

--- app.js
+++ app.js
@@ -18,10 +18,11 @@ class Particle {
   /**
   * ! Draw method for particle. Creting outside constructor saves resources. */
   draw() {
+    let x = Math.cos(this.position) * this.moveRadius + canvas.width / 2;
+    let y = Math.sin(this.position) * this.moveRadius + canvas.height / 2;
     ctx.beginPath();
-    ctx.arc(Math.cos(this.position) * this.moveRadius + canvas.width / 2,
-    Math.sin(this.position) * this.moveRadius + canvas.height / 2,
-    this.size, 0, Math.PI * 2);
+    // ctx.arc(x, y, this.moveRadius / 15, 0, Math.PI * 2);
+    drawStar(x, y, 6, this.size, this.size / 2);
     ctx.closePath();
     ctx.fillStyle = 'white';
     ctx.fill();
@@ -33,15 +34,15 @@ class Particle {
     this.position += this.step;
     this.draw();
   }
-  
 }
+
 function init() {
   particleArray = [];
   for (let i = 0; i < 500; i++) {
     let moveRadius = Math.random() * canvas.width;
     let step = (Math.random() * 0.002) + 0.002;
     let position = Math.random() * (Math.PI * 2);
-    let size = (Math.random() * 8) + 0.5;
+    let size = (Math.random() * 8) + 15;
     particleArray.push(new Particle(moveRadius, step, position, size));
   }
 }
@@ -63,6 +64,29 @@ window.addEventListener('resize', function() {
   canvas.height = window.innerHeight;
   init();
 });
+
+function drawStar(positionX, positionY, spikes, outerRadius, innerRadius) {
+  let rotation = Math.PI / 2 * 3;
+  let x = positionX;
+  let y = positionY;
+  let step = Math.PI / spikes;
+
+  ctx.beginPath();
+  ctx.moveTo(positionX, positionY - outerRadius);
+  for(let i = 0; i < spikes; i++) {
+    x = positionX + Math.cos(rotation) * outerRadius;
+    y = positionY + Math.sin(rotation) * outerRadius;
+    ctx.lineTo(x, y);
+    rotation += step;
+
+    x = positionX + Math.cos(rotation) * innerRadius;
+    y = positionY + Math.sin(rotation) * innerRadius;
+    ctx.lineTo(x, y);
+    rotation += step;
+  }
+  ctx.lineTo(positionX, positionY - outerRadius);
+  ctx.closePath();
+}
 
 init();
 animate();
